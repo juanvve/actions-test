e9387d04456131a417b9208ddbde6d27678f5979
juanvve <juanv.solinfo@gmail.com>
2024-07-26 13:03:41 -0500

    cambio de vars

--- hola-lenguaje.js
+++ hola-lenguaje.js
@@ -2,8 +2,8 @@ var nombre
 var lenfavorito
 
 function holaLenguaje() {
-  const nombre = process.env.USERNAME;
-  const lenfavorito = process.env.WORKFLOW;
+  const nombre = process.env.NAME;
+  const lenfavorito = process.env.LANGUAGE;
   console.log(`¡Hola, ${nombre} desde GitHub Actions!`);
   console.log(`Estoy Usando mi Lenguaje Favorito ${lenfavorito}`);
   console.log(process.env);
