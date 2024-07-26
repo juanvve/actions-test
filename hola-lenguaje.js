b20d56ac1ccfe332ebdf87755fa57407cff298aa
juanvve <juanv.solinfo@gmail.com>
2024-07-26 12:35:24 -0500

    se crea trigger y archivo js adicional

---
+++ hola-lenguaje.js
@@ -0,0 +1,11 @@
+var nombre
+var lenfavorito
+
+function holaLenguaje() {
+  const nombre = process.env.USERNAME;
+  const lenfavorito = process.env.WORKFLOW;
+  console.log(`¡Hola, ${nombre} desde GitHub Actions!`);
+  console.log(`Estoy Usando mi Lenguaje Favorito ${lenfavorito}`);
+}
+
+holaLenguaje();
